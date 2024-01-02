9105f5507e45f9a1c3189172ba94c1bd20a31f6a
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 16:11:14 +0200

    Create routes for follow controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "posts#index"
   devise_for :users
-  
+
   resources :users do
     resources :posts
   end
+  resources :follows
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
