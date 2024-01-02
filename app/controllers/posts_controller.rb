ede2373d9c127134ec04c79296d2f18d255596b4
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 15:29:08 +0200

    Code the post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ApplicationController
         @post = Post.new
     end
 
+    def create
+        @user = User.find(params[:id])
+        @post = @user.posts.build(post_params)
+
+        if @post.save
+            redirect_to :root, status: :see_other
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:body,:author_id)
+    end
 end
