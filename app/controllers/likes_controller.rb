88a23b2ec9dfcbc31c9b74f29b987abe390a784e
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 17:37:42 +0200

    Create a like object

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+    def create
+        @post = Post.find(params[:like][:liked_post_id])
+        @like = @post.likes.build(like_params)
+
+        if @like.save
+            redirect_to :root, status: :see_other
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def like_params
+        params.require(:like).permit(:liked_post_id, :liking_user_id)
+    end
 end
