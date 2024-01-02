582e907de86949d73371cb0952033d5fb5f42350
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 14:19:17 +0200

    Add dependent:destory option in post associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
     validates :body, presence: true
 
-    has_many :likes, foreign_key: :liked_post_id
-    has_many :users_who_liked_this, through: :likes, source: :liking_user
+    has_many :likes, foreign_key: :liked_post_id, dependent: :destroy
+    has_many :users_who_liked_this, through: :likes, source: :liking_user, dependent: :destroy
     belongs_to :author, foreign_key: :author_id, class_name: "User"
 end
