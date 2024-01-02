75d9297f6ed6c42db02b9f620c41072d4c3cc097
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 13:43:39 +0200

    Set up association between users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     validates :body, presence: true
 
     has_many :likes, foreign_key: :liked_post_id
+    has_many :users_who_liked_this, through: :likes, source: :liking_user
+    belongs_to :author, foreign_key: :author_id, class_name: "User"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
     has_many :followings, through: :given_follows, source: :followed_user
 
     has_many :likes, foreign_key: :liking_user_id
+    has_many :liked_posts, through: :likes, source: :liked_post
+    has_many :authored_posts, foreign_key: :author_id, class_name: "Post"
 end
