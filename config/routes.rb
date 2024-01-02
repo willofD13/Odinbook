6b022d5271e5016aa93155f53f2783e2cd7d2fb8
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 16:03:04 +0200

    Nest posts resources under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "posts#index"
   devise_for :users
-  resources :users
+  
+  resources :users do
+    resources :posts
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
