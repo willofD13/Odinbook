137107640755a380f973ba9aaeb7c252c98a6f11
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 14:18:28 +0200

    Add dependent:destroy option in user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true 
     validates :email, presence: true , uniqueness: true
     
-    has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
-    has_many :followers, through: :received_follows, source: :follower
+    has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow", dependent: :destroy
+    has_many :followers, through: :received_follows, source: :follower, dependent: :destroy
 
-    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
-    has_many :followings, through: :given_follows, source: :followed_user
+    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
+    has_many :followings, through: :given_follows, source: :followed_user, dependent: :destroy
 
-    has_many :likes, foreign_key: :liking_user_id
-    has_many :liked_posts, through: :likes, source: :liked_post
-    has_many :authored_posts, foreign_key: :author_id, class_name: "Post"
+    has_many :likes, foreign_key: :liking_user_id, dependent: :destroy
+    has_many :liked_posts, through: :likes, source: :liked_post, dependent: :destroy
+    has_many :authored_posts, foreign_key: :author_id, class_name: "Post", dependent: :destroy
 end
