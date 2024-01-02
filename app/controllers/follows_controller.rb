4f5db58c59740875c9e1ca6a3e860be454acec86
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 17:18:00 +0200

    Create a new follow object

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,22 @@
 class FollowsController < ApplicationController
+    def new
+        @follow = Follow.new
+    end
+
+
+    def create
+        @user = User.find(params[:follow][:followed_user_id])
+        @follow = @user.received_follows.build(follow_params)
+
+        if @follow.save
+            redirect_to :root, status: :see_other
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def follow_params
+        params.require(:follow).permit(:follower_id, :followed_user_id)
+    end
 end
