1de62e2d02c528d8320f1e0738aea61a53b629af
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 13:03:23 +0200

    Set up association between user and follow model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
     validates :name, presence: true, uniqueness: true 
-    validates :email, presence: true , uniqueness: true 
+    validates :email, presence: true , uniqueness: true
+    
+    has_many :received_follows, foreign_key: :followed_user_id, class_name: "Follow"
+    has_many :followers, through: :received_follows, source: :follower
+
+    has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+    has_many :followings, through: :given_follows, source: :followed_user
+
+    
 end
