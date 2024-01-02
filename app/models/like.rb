7416d511b015adf1c3de216cc4072068c91042bb
willofD13 <leonidastsiakas13@gmail.com>
2024-01-02 13:21:13 +0200

    Set up association between post and like models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,4 @@
 class Like < ApplicationRecord
     belongs_to :liking_user, foreign_key: :liking_user_id, class_name: "User"
+    belongs_to :liked_post, foreign_key: :liked_post_id, class_name: "Post"
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     validates :body, presence: true
+
+    has_many :likes, foreign_key: :liked_post_id
 end
