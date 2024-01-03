8e9ba64c75d29ab9956fb3c3d8c8567e5dbab603
willofD13 <leonidastsiakas13@gmail.com>
2024-01-03 16:27:10 +0200

    Nest comments routes under users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :users do
     resources :posts
+    resources :comments
   end
 
   resources :users do
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
 
   resources :posts do
     resources :likes
+    resources :comments
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
