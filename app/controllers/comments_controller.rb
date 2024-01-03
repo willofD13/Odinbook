e507fe27a9dd8f94ee13de80b03e29555c8d095c
willofD13 <leonidastsiakas13@gmail.com>
2024-01-03 17:52:13 +0200

    Store the post object in a variable and create destroy action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,21 @@
 class CommentsController < ApplicationController
     def create
-        @user = User.find(params[:user_id])
+        @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
         redirect_to :root, status: :see_other
     end
 
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+
+        redirect_to :root, status: :see_other
+    end
+
+    private
     def comment_params
         params.require(:comment).permit(:body,:user_id)
     end
+
+    
 end
