e3f0c265e4ec8ab8ffccfa0ed53925dd2f652b38
willofD13 <leonidastsiakas13@gmail.com>
2024-01-03 17:49:56 +0200

    Add has many association with comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     validates :body, presence: true
 
+    has_many :comments, dependent: :destroy
     has_many :likes, foreign_key: :liked_post_id, dependent: :destroy
     has_many :users_who_liked_this, through: :likes, source: :liking_user, dependent: :destroy
     belongs_to :author, foreign_key: :author_id, class_name: "User"
