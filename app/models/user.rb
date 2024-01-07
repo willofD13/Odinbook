4da5d7a86a51057c605b7aa0f212e8de93fb1b8e
willofD13 <leonidastsiakas13@gmail.com>
2024-01-07 17:16:55 +0200

    Add omniauthable strategy to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-    validates :name, presence: true, uniqueness: true 
+           :recoverable, :rememberable, :validatable,
+           :omniauthable, omniauth_providers: [:github]
+
+    validates :name,presence: true, uniqueness: true 
     validates :email, presence: true , uniqueness: true
 
     has_many :comments, dependent: :destroy
@@ -17,4 +19,18 @@ class User < ApplicationRecord
     has_many :likes, foreign_key: :liking_user_id, dependent: :destroy
     has_many :liked_posts, through: :likes, source: :liked_post, dependent: :destroy
     has_many :authored_posts, foreign_key: :author_id, class_name: "Post", dependent: :destroy
+
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(email: data['email']).first
+  
+      # Uncomment the section below if you want users to be created if they don't exist
+       unless user
+           user = User.create(name: data['name'],
+              email: data['email'],
+              password: Devise.friendly_token[0,20]
+           )
+       end
+      user
+  end
 end
