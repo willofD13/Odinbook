c65543fb1e021dec4a39cb0728ddde472442c7d6
willofD13 <leonidastsiakas13@gmail.com>
2024-01-07 17:13:37 +0200

    Set up omniauth configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -269,9 +269,10 @@ Devise.setup do |config|
   config.sign_out_via = :delete
 
   # ==> OmniAuth
+
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+   config.omniauth :github, ENV['ODINBOOK_GITHUB_KEY'], ENV['ODINBOOK_GITHUB_SECRET'], {}
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
