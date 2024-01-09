92ee47c1f4fe7500ec16d1029e90595d51157ecf
willofD13 <leonidastsiakas13@gmail.com>
2024-01-09 19:04:35 +0200

    Create edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,25 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+
+        if @user.save
+            redirect_to user_path(@user.id)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:avatar)
+    end
     
 end
