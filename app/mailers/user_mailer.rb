8bf938d748bf7cb7f4ced994173c8644b73980a4
willofD13 <leonidastsiakas13@gmail.com>
2024-01-09 19:53:02 +0200

    Create welcome email action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+    def welcome_email
+        @user = params[:user]
+        @url  = 'http://localhost:3000/users/sign_in'
+        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    end
 end
