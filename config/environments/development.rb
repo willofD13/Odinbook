f4d2c89aee9cfe8916bf7c1d91152d6febfc8cb9
willofD13 <leonidastsiakas13@gmail.com>
2024-01-18 22:22:50 +0200

    Set smtp configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587 ,
+    domain: 'localhost:3000',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: 'leonidatsiakas13@gmail.com',
+    password: ENV['GMAIL_ODINBOOK_PASSWORD']
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
