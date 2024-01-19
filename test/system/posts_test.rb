0440f4265c4ad8b7dd95802938ec1e1ad0785896
willofD13 <leonidastsiakas13@gmail.com>
2024-01-19 20:05:11 +0200

    Test post creation

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -1,9 +1,23 @@
 require "application_system_test_case"
 
 class PostsTest < ApplicationSystemTestCase
+  
    test "visiting the index" do
      visit posts_path
   
      assert_selector "h2", text: "Log in"
    end
+
+   test "creating a post" do
+    user = users(:one)
+    sign_in user 
+
+    visit root_path
+
+    fill_in "post[body]", with: "Testing posts!"
+
+    click_on "Create post"
+ 
+    assert_text "Testing posts!"
+  end
 end
