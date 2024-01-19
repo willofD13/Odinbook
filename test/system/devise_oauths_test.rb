cd22adaabbd124646e8564f3a1f9e42857197676
willofD13 <leonidastsiakas13@gmail.com>
2024-01-19 18:02:42 +0200

    Test user creation and sign in

--- test/system/devise_oauths_test.rb
+++ test/system/devise_oauths_test.rb
@@ -14,4 +14,25 @@ class DeviseOauthsTest < ApplicationSystemTestCase
     visit root_path
     assert_current_path root_path
   end
+
+  test 'creating a user and sign in' do 
+    name = Faker::Name.name 
+    email = Faker::Internet.email 
+    password = Faker::Internet.password(min_length: 10, max_length: 30)
+    
+
+    visit root_path
+
+    click_on 'Sign up'
+
+    fill_in 'Name', with: name
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+
+
+    click_button 'Sign up'
+
+    assert_current_path root_path
+  end
 end
