b97fa0af618c4229aa767dad6091b3501a9a53a8
willofD13 <leonidastsiakas13@gmail.com>
2024-01-20 20:33:35 +0200

    Write integration test for post creation

--- test/integration/odinbook_flow_test.rb
+++ test/integration/odinbook_flow_test.rb
@@ -12,4 +12,20 @@ class OdinbookFlowTest < ActionDispatch::IntegrationTest
 
     assert_select 'h3', 'Users you follow:'
   end
+
+  test 'post creation' do
+    user = users(:one)
+    sign_in user
+
+    get '/'
+    get "/posts/new"
+    assert_response :success
+
+    post posts_url,
+      params: { 'post'=> { 'body'=> "post successfull", 'author_id'=> users(:one).id } }, as: :turbo_stream
+    
+    assert_response :success
+    assert_select "strong", "post successfull"
+
+  end
 end
