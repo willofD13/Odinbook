051ba2896882e336110db7170175a9a28b45e051
willofD13 <leonidastsiakas13@gmail.com>
2024-01-20 14:42:09 +0200

    Test follow creation

--- test/models/follow_test.rb
+++ test/models/follow_test.rb
@@ -4,4 +4,8 @@ class FollowTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'follow creation' do 
+    follow = Follow.new
+    assert_not follow.save
+  end
 end
